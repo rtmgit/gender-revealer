ae8b20d3836e85ebbc3ec90eeeaa71f864fbb63f
rtmgit <raviteja.mudunuri@gmail.com>
2020-08-29 13:43:21 +0530

    Updates

---
+++ precache-manifest.5f965b644cfec59fc9b9a9c5f8687805.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc22384eb6f05f1fc717c8ce2941e7c",
+    "url": "/gender-revealer/index.html"
+  },
+  {
+    "revision": "3949cfe604ca1b7dbb6a",
+    "url": "/gender-revealer/static/css/main.31c7cf88.chunk.css"
+  },
+  {
+    "revision": "c341ab17e0c6ada86a8e",
+    "url": "/gender-revealer/static/js/2.b3c2b3a9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gender-revealer/static/js/2.b3c2b3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3949cfe604ca1b7dbb6a",
+    "url": "/gender-revealer/static/js/main.ffce00da.chunk.js"
+  },
+  {
+    "revision": "c9e8df71d8958f452624",
+    "url": "/gender-revealer/static/js/runtime-main.fafe87d0.js"
+  },
+  {
+    "revision": "d3dc316775c6d30c54d08bd2155b8741",
+    "url": "/gender-revealer/static/media/boy.d3dc3167.png"
+  },
+  {
+    "revision": "65f4bbef5de7425172e687c419961d99",
+    "url": "/gender-revealer/static/media/family.65f4bbef.jpg"
+  },
+  {
+    "revision": "f12879d0be71c5edd207952aa2a28e6a",
+    "url": "/gender-revealer/static/media/girl.f12879d0.png"
+  }
+]);
